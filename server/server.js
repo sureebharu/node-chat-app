e249c8301d8ef14e7685fb53af55f6cc96bbc361
Bharathi S <bharathi.selvarajan@gmail.com>
2019-01-02 11:27:40 +0530

    web scokets included

--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
-const app = express();
+const socketIO = require("socket.io");
+
+
 const port = process.env.PORT||3000;
-
 const PathPublic = path.join(__dirname,"../public");
+const app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 app.use(express.static(PathPublic));
-// app.get('/',(req,res)=>{
+io.on('connection',(socket)=>{
+ console.log('New user connected');
 
-// });
-
-app.listen(port,()=>{
+ socket.on('disconnect',()=>{
+     console.log('user disconnected');
+ })
+})
+server.listen(port,()=>{
     console.log("Server is up on port 3000");
 });
